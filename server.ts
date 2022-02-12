3fd774f7ebfd750ac1c69af2ddedcb6705f06868
Masayuki Onishi <omccello@gmail.com>
2022-02-12 23:14:29 +0900

    get list & detail

--- server.ts
+++ server.ts
@@ -4,11 +4,60 @@ const app = new Application();
 const port = 5000;
 const router = new Router();
 
+let activities = [
+  {
+    activity: 'Learn how to write in shorthand',
+    accessibility: 0.1,
+    type: 'education',
+    participants: 1,
+    price: '',
+    link: '',
+    id: '6778219',
+  },
+  {
+    activity: 'Learn how to french braid hair',
+    accessibility: 0.1,
+    type: 'education',
+    participants: 1,
+    price: '',
+    link: '',
+    id: '8926492',
+  },
+  {
+    activity: 'Compliment someone',
+    accessibility: 0.0,
+    type: 'social',
+    participants: 2,
+    price: '',
+    link: '',
+    id: '9149470',
+  },
+];
+
 router.get('/api/v1/activities', ({ response }: { response: any }) => {
   response.body = {
     success: true,
-    data: 'activities',
+    data: activities,
   };
+});
+
+router.get('/api/v1/activities/:id', ({ params, response }: { params: { id: string }; response: any }) => {
+  const activity = activities.find((p) => p.id === params.id);
+  console.log(activity);
+
+  if (activity) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: activity,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: true,
+      data: 'Not found Activity.',
+    };
+  }
 });
 
 app.use(router.routes());
